3423862279ba72b3b1980fdf5c5bef2063f5cce4
Alex Teper <67521917+AlexTeper99@users.noreply.github.com>
2023-07-29 18:59:33 -0300

    change index message

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express"; // ESModules
+import express from "express";
 require("../mongo");
 
 import characterRouter from "./routes/characters";
@@ -7,14 +7,15 @@ const app = express();
 const cors = require("cors");
 
 app.use(cors());
-app.use(express.json()); // middleware que transforma la req.body a un json
+app.use(express.json());
 
 const PORT = process.env.PORT;
 
-// el guion bajo es para no usarla.
 app.get("/", (_req, res) => {
   console.log("someone pinged here!!");
-  res.send("Bienvenido a la api de valorant. Accede a /api/characters");
+  res.send(
+    "Welcome to my Valorant Api. Access to /api/characters or to /api/characters/:id"
+  );
 });
 
 app.use("/api/characters", characterRouter);
