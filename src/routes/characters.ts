58890953dee44d303d0984a0af715b0c65ae424d
Alex Teper <67521917+AlexTeper99@users.noreply.github.com>
2023-07-29 18:47:48 -0300

    endpoint to post multiple agents from valorant api

--- src/routes/characters.ts
+++ src/routes/characters.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { Character } from "../models";
 import { handleErrors } from "../middlewares";
 import { toNewCharacter } from "../utils";
+import axios from "axios";
+import { ISkill, NewCharacterEntry } from "../types";
 
 const router = express.Router();
 
@@ -53,6 +55,67 @@ router.post("/", async (req, res, next) => {
     newCharacter.save().then((savedCharacter) => {
       res.json(savedCharacter);
     });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/post/agents", async (_req, res, next) => {
+  try {
+    const apiUrl =
+      "https://valorant-api.com/v1/agents?isPlayableCharacter=true";
+    const response = await axios.get(apiUrl);
+
+    const characters = response.data.data;
+
+    characters.map(async (character: any) => {
+      let makeSkillsFormat: ISkill[] = [];
+
+      await character?.abilities.forEach((ability: any) => {
+        makeSkillsFormat.push({
+          key: ability.slot,
+          description: ability.description,
+          name: ability.displayName,
+          icon: ability.displayIcon,
+        });
+      });
+
+      const passCharacter: NewCharacterEntry = {
+        name: character.displayName,
+        bio: character.description,
+        rol: character.role.displayName,
+        image: character.fullPortrait,
+        background: character.background,
+        displayIcon: character.displayIcon,
+        backgroundGradientColors: character.backgroundGradientColors,
+        skills: makeSkillsFormat,
+      };
+
+      const auxCharacter = toNewCharacter(passCharacter); //validation
+      const newCharacter = new Character({
+        name: auxCharacter.name,
+        rol: auxCharacter.rol,
+        bio: auxCharacter.bio,
+        image: auxCharacter.image,
+        background: auxCharacter.background,
+        displayIcon: auxCharacter.displayIcon,
+        backgroundGradientColors: auxCharacter.backgroundGradientColors,
+        skills: auxCharacter.skills,
+      });
+      const characterAlredyExist = await Character.findOne({
+        name: newCharacter.name,
+      });
+      if (characterAlredyExist) {
+        return res.status(409).send("Character alredy exist");
+      }
+      newCharacter.save().then(() => {
+        // res.json(savedCharacter);
+
+        makeSkillsFormat = [];
+      });
+    });
+
+    res.send("We add the agents to the database from valorant api");
   } catch (err) {
     next(err);
   }
--- src/utils/characterUtils.ts
+++ src/utils/characterUtils.ts
@@ -51,23 +51,26 @@ function isSkill(object: any): object is ISkill {
     "key" in object &&
     "name" in object &&
     "description" in object &&
+    "icon" in object &&
     typeof object.key === "string" &&
     typeof object.name === "string" &&
     typeof object.description === "string" &&
-    (typeof object.icon === "undefined" || typeof object.icon === "string") &&
-    (typeof object.video === "undefined" || typeof object.video === "string")
+    (typeof object.icon === "undefined" ||
+      typeof object.icon === "string" ||
+      typeof object.icon !== null) &&
+    (typeof object.video === "undefined" ||
+      typeof object.video === "string" ||
+      typeof object.video !== null)
   );
 }
 
 function addConditionalAttribute(attribute: any, type: any): boolean {
   let aux = typeof attribute !== "undefined" && typeof attribute === type;
 
-  console.log(aux);
-
   return aux;
 }
 
-function parseSkills(skills: any): ISkill[] {
+function parseSkills(skills: any, characterName: any): ISkill[] {
   if (Array.isArray(skills)) {
     let i: number = 0;
     let isSkillFormat = true;
@@ -76,7 +79,13 @@ function parseSkills(skills: any): ISkill[] {
       isSkillFormat = isSkill(skills[i]);
 
       if (!isSkillFormat) {
-        throw new Error("Element " + i + " is not Skill Format");
+        throw new Error(
+          "Element " +
+            i +
+            " from character " +
+            characterName +
+            " is not Skill Format"
+        );
       }
 
       i++;
@@ -122,10 +131,10 @@ const parseBackgroundGradientColors = (
 
 export const toNewCharacter = (object: any): NewCharacterEntry => {
   let newCharacter: NewCharacterEntry = {
-    bio: parseBio(object.bio),
     name: parseName(object.name),
+    bio: parseBio(object.bio),
     rol: parseRol(object.rol),
-    skills: parseSkills(object.skills),
+    skills: parseSkills(object.skills, object.name),
     image: parseString(object.image, "Image must be a string"),
     background: parseString(object.background, "Background must be a string"),
     displayIcon: parseString(object.displayIcon, "Image must be a string"),
@@ -137,7 +146,6 @@ export const toNewCharacter = (object: any): NewCharacterEntry => {
   //WHY THIS ISNT WORKING?
   if (addConditionalAttribute(object.background, "string")) {
     newCharacter.background = object.background;
-    console.log(newCharacter.background);
   }
 
   if (addConditionalAttribute(object.image, "string")) {
